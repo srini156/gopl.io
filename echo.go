bb8164d91c525c8d1b64a5813399f93e30cf9a75
Srinivas Iyengar <srini156@gmail.com>
2016-10-16 13:10:13 +0530

    1.2 example - echo commandline args

---
+++ echo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(echo3())
+}
+
+func echo1() string {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	return s
+}
+
+func echo2() string {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	return s
+}
+
+func echo3() string {
+	return strings.Join(os.Args[1:], " ")
+}
